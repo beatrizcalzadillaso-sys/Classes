69654aa02f6a3cee20ce5a978b411318c01567b1
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-04 17:53:50 +0100

    finish asking persona's attributes

--- src/exercises6/ExercisePersona.java
+++ src/exercises6/ExercisePersona.java
@@ -16,26 +16,29 @@ public class ExercisePersona {
 		Persona uno= new Persona();
 		Persona dos= new Persona();
 		
-		System.out.println("Teclee el nombre de persona Uno");
+		System.out.print("Teclee el nombre de persona Uno: ");
 		uno.setNombre(keyboard.nextLine());
-		System.out.println("Teclee el apellido de persona Uno");
+		System.out.print("\nTeclee el apellido de persona Uno: ");
 		uno.setApellidos(keyboard.nextLine());
-		System.out.println("Teclee la edad de persona Uno");
+		System.out.print("\nTeclee la edad de persona Uno: ");
 		uno.setEdad(Integer.parseInt(keyboard.nextLine()));
-		System.out.println("Teclee si true/false persona Uno es casada o no");
+		System.out.print("\nTeclee si true/false persona Uno es casada o no: ");
 		uno.setCasado(Boolean.parseBoolean(keyboard.nextLine()));
-		System.out.println("Teclee el DNI de la persona 1");
+		System.out.print("\nTeclee el DNI de la persona 1:");
+		uno.setNumDocIdent(keyboard.nextLine());
+		
 		
 		// PERSONA 2
-		System.out.println("Teclee el nombre de persona Dos");
+		System.out.print("Teclee el nombre de persona Dos");
 		dos.setNombre(keyboard.nextLine());
-		System.out.println("Teclee el apellido de persona Dos");
+		System.out.print("\nTeclee el apellido de persona Dos");
 		dos.setApellidos(keyboard.nextLine());
-		System.out.println("Teclee la edad de persona Uno");
+		System.out.print("\nTeclee la edad de persona Uno");
 		dos.setEdad(Integer.parseInt(keyboard.nextLine()));
-		System.out.println("Teclee si true/false persona Dos es casada o no");
+		System.out.print("\nTeclee si true/false persona Dos es casada o no");
 		dos.setCasado(Boolean.parseBoolean(keyboard.nextLine()));
-		System.out.println("Teclee el DNI de la persona 1");
+		System.out.print("\nTeclee el DNI de la persona 1");
+		dos.setNumDocIdent(keyboard.nextLine());
 	}
 }
 
